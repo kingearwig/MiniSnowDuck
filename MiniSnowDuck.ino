97913843f1cdcaf4b79fec485dc7e861044e90f5
kingearwig <33814686+kingearwig@users.noreply.github.com>
2019-02-20 20:09:52 -0600

    Update MiniSnowDuck.ino

--- MiniSnowDuck.ino
+++ MiniSnowDuck.ino
@@ -5,72 +5,126 @@
 #define TailLEDcount 9
 #define RWingLEDcount 2
 #define LWingLEDcount 2
+#define ch5PIN D4
+Adafruit_NeoPixel Tail = Adafruit_NeoPixel(TailLEDcount, TailPIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel RWing = Adafruit_NeoPixel(RWingLEDcount, RWingPIN, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel LWing = Adafruit_NeoPixel(LWingLEDcount, LWingPIN, NEO_GRBW + NEO_KHZ800);
+
+int ch5;
 int brightness=255;
-Adafruit_NeoPixel Tail = Adafruit_NeoPixel(TailLEDcount,TailPIN,NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel RWing = Adafruit_NeoPixel(RWingLEDcount,RWingPIN,NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel LWing = Adafruit_NeoPixel(LWingLEDcount,LWingPIN,NEO_GRBW + NEO_KHZ800);
-unsigned long StrobeLength=5;
-unsigned long StrobeSpeed=100;
-unsigned long lasttime=millis();
-bool TailState=0;
+unsigned long NOW=millis();
+unsigned long LAST=millis();
+unsigned long interval=500;
+unsigned long strobelength=10;
+int strobestate=1;
 void setup() {
   // put your setup code here, to run once:
-  for(int i=0;i<TailLEDcount;i++){
-    Tail.setPixelColor(i,0,0,0,255);
-  }
-  LWing.setPixelColor(1,255,255,255,255);
-  LWing.setPixelColor(0,0,255,0,0);
-  LWing.show();
-  RWing.setPixelColor(1,255,255,255,255);
-  RWing.setPixelColor(0,255,0,0,0);
-  RWing.show();
-  
-  Tail.setBrightness(brightness);
-  Tail.begin();
-  Tail.show();
-  TailState=1;
-  RWing.setBrightness(brightness);
+  Serial.begin(115200);
+  pinMode(ch5PIN,INPUT);
   RWing.begin();
+  LWing.begin();
+  Tail.begin();
+  LWing.setPixelColor(0,255,255,255,255);
+  LWing.setPixelColor(1,0,255,0,0);
+  RWing.setPixelColor(0,255,255,255,255);
+  RWing.setPixelColor(1,255,0,0,0);
+  RWing.setBrightness(brightness);
   RWing.show();
   LWing.setBrightness(brightness);
-  LWing.begin();
   LWing.show();
-  Serial.begin(115200);
+  
+  for(int i=0;i<TailLEDcount;i++){
+    Tail.setPixelColor(i,0,0,0,0);
+  }
+  Tail.setBrightness(brightness);
+  Tail.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //nav();
-  strobe();
-  //Serial.println(millis()-lasttime);
-  Serial.println(TailState);
+  NOW=millis();
+  nav();
+  ch5=pulseIn(ch5PIN,HIGH,25000);
+  if(ch5>1200){
+    brightness=map(ch5,1200,2000,255,0);
+    strobe();
+  }
+  if(ch5<1200){
+    brightness=map(ch5,1200,2000,255,0);
+    solid();
+  }
+  Serial.print(ch5);Serial.print(" ");Serial.println(brightness);
+}
+
+void disco(){
+  
 }
 
 void nav(){
   LWing.setPixelColor(0,255,255,255,255);
   LWing.setPixelColor(1,0,255,0,0);
-  LWing.show();
   RWing.setPixelColor(0,255,255,255,255);
   RWing.setPixelColor(1,255,0,0,0);
+  RWing.setBrightness(brightness);
   RWing.show();
+  LWing.setBrightness(brightness);
+  LWing.show();
 }
-void strobe(){
-  unsigned long currentmillis=millis();
-  if(TailState=0 && currentmillis-lasttime>StrobeSpeed){
-    for(int i=0;i<TailLEDcount;i++){
-      Tail.setPixelColor(i,255,255,255,255);
-    }
-    Tail.show();
-    lasttime=millis();
-    TailState=1;
-    
+
+void solid(){
+  for(int i=0;i<TailLEDcount;i++){
+    Tail.setPixelColor(i,255,255,255,255);
   }
-  if(TailState=1 && currentmillis-lasttime>StrobeLength){
-    for(int i=0;i<TailLEDcount;i++){
-      Tail.setPixelColor(i,0,0,0,0);
-    }
-    Tail.show();
-    lasttime=millis();
-    TailState=0;
+  Tail.setBrightness(255);
+  Tail.show();
+}
+
+
+void strobe(){
+  switch(strobestate){
+    case 0:
+      if(NOW-LAST>interval){
+        LAST=millis();
+        strobestate=1;
+        for(int i=0;i<TailLEDcount;i++){
+          Tail.setPixelColor(i,255,255,255,255);
+        }
+        Tail.setBrightness(brightness);
+        Tail.show();
+      }
+    break;
+    case 1:
+      if(NOW-LAST>strobelength){
+        LAST=millis();
+        strobestate=2;
+        for(int i=0;i<TailLEDcount;i++){
+          Tail.setPixelColor(i,0,0,0,0);
+        }
+      Tail.setBrightness(brightness);
+      Tail.show();
+      }
+    break;
+    case 2:
+      if(NOW-LAST>interval/4){
+        LAST=millis();
+        strobestate=3;
+        for(int i=0;i<TailLEDcount;i++){
+          Tail.setPixelColor(i,255,255,255,255);
+        }
+      Tail.setBrightness(brightness);
+      Tail.show();
+      }      
+    break;
+    case 3:
+      if(NOW-LAST>strobelength){
+        LAST=millis();
+        strobestate=0;
+        for(int i=0;i<TailLEDcount;i++){
+          Tail.setPixelColor(i,0,0,0,0);
+        }
+      Tail.setBrightness(brightness);
+      Tail.show();
+      }      
+    break;
   }
 }
